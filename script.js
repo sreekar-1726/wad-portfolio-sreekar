8bf1fb7787b8a407a59fef7b08cbe5cf7b20b450
sreekar-1726 <101689100+sreekar-1726@users.noreply.github.com>
2024-04-04 15:00:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        const homeSection = document.getElementById('home');
+        homeSection.classList.add('fade-in');
+    }, 3000); // Delay in milliseconds (3 seconds)
+});
